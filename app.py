e958a327fa37ac421f725f5a3424039d20e7529d
KN161725 <137528188+KN161725@users.noreply.github.com>
2023-11-04 21:52:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask
 app=Flask(__name__)
 import route
 route.set_route(app)
+app.secret_key=route.secret_key(58)
 if __name__=="__main__":
-    app.secret_key=route.secret_key(58)
     app.run(debug=True)
-
